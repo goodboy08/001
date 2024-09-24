42ac34207a6c38c959f8807e0d23b282cf7e254c
goodboy08 <goodboy5508741@126.com>
2024-09-24 17:14:25 +0800

    Add files via upload

--- text2/9.列表生成式.py
+++ text2/9.列表生成式.py
@@ -1,3 +1,9 @@
+# ·列表生成式
+#    -生成指定范围的数值列表
+#        Ist=[expression for item in range]
+#    -从列表中选择符合条件的元素组成新的列表
+#           Ist=[expression for item in 列表 if condition]
+
 import random
 
 lst=[item for item in range(1,11)]
