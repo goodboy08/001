969c3e1e6007eaac9feeb69ccd13b6668a15ffab
goodboy08 <goodboy5508741@126.com>
2024-09-24 17:12:36 +0800

    Add files via upload

--- text2/8.列表的排序操作.py
+++ text2/8.列表的排序操作.py
@@ -8,3 +8,18 @@ print('列表升序:',lst)
 # 降序
 lst.sort(reverse=True)
 print('列表降序:',lst)
+
+print('------------------------------------------')
+lst2=['banana','apple','Cat','Orange']
+# 升序，先排大写，再排小写
+lst2.sort()
+print(lst2)
+# 降序，先排小写，再排大写
+lst2.sort(reverse=True)
+print(lst2)
+# 忽略大小写进行比较
+lst2.sort(key=str.lower)
+print(lst2)
+# 忽略大小写，降序
+lst2.sort(key=str.lower,reverse=True)
+print(lst2)
