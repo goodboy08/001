2c08e2e765a7fb27982329ea65cdb76c5fcf8ca6
goodboy08 <goodboy5508741@126.com>
2024-09-27 16:21:25 +0800

    Add files via upload

--- chap2/4.变量的定义和使用.py
+++ chap2/4.变量的定义和使用.py
@@ -3,12 +3,16 @@ luck_number=8
 
 my_name='王小二'   #字符串类型的变量
 print(my_name,'的幸运数字为:',luck_number,sep='')
+# 王小二的幸运数字为:8
+
 print('luck_number的数据类型是:',type(luck_number))
+# luck_number的数据类型是: <class 'int'>
 
 #Python动态修改变量的数据类型，通过赋不同类型的值直接修改
 #变量的值可以更改
 luck_number='北京欢迎你'
 print('luck_number的数据类型是:',type(luck_number))
+# luck_number的数据类型是: <class 'str'>
 
 #Python允许多个变量指向同一个值
 no=number=1024
